0960deace04cd76973c1f24f764e91ad46248032
Kaito <kaijotic9ie@gmail.com>
2025-01-02 01:23:58 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useNextSeoProps } from './config/useNextSeoProps'
 
 const config: DocsThemeConfig = {
-  logo: <span>b5h22's Dev Notes</span>,
+  logo: <span>Kaijotic's Dev Notes</span>,
   project: {
-    link: 'https://github.com/b5h22/dev-notes',
+    link: 'https://github.com/Kaijotic/dev-notes',
   },
-  docsRepositoryBase: 'https://github.com/b5h22/dev-notes/tree/main',
+  docsRepositoryBase: 'https://github.com/Kaijotic/dev-notes/tree/main',
   footer: {
-    text: '© 2023 - b5h22 Powered by Nextra, Github Pages',
+    text: '© 2023 - Kaijotic Powered by Nextra, Github Pages',
   },
   feedback: {
     content: () => <>Question? Give me feedback →</>,
